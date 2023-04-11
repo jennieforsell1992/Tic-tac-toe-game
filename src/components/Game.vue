ba4e39ce70b1f13af914f748508b2c1b1a272352
Jennie <forselljennie@gmail.com>
2023-04-11 11:07:25 +0200

    (feat): created 3 components in folder components and linked every components to eachother

---
+++ src/components/Game.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import InsertUser from "./InsertUser.vue";
+import TickTacToe from "./TickTacToe.vue";
+</script>
+
+<template>
+    <InsertUser></InsertUser>
+    <TickTacToe></TickTacToe>
+</template>
+
+<style scoped></style>
