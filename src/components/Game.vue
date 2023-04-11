088d298edc84d5856d4ac45d23c0defb15ccf0c6
Jennie <forselljennie@gmail.com>
2023-04-11 12:06:56 +0200

    (feat): created a state with a empty list in Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import InsertUser from "./InsertUser.vue";
 import TickTacToe from "./TickTacToe.vue";
+import { ref } from "vue";
+import { User } from "../models/User";
+
+const user = ref<User[]>([])
+
 </script>
 
 <template>
