10118872bc9ae2d2ff31aeb18ea88d57aebad37b
Jennie <forselljennie@gmail.com>
2023-04-17 10:27:22 +0200

    deleted code in tictactoe.vue that I don't use

--- src/components/TickTacToe.vue
+++ src/components/TickTacToe.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { User } from "../models/User";
-// import { computed } from "@vue/reactivity";
 
 
 interface ITicTacToeProps {
@@ -55,9 +54,7 @@ const winnerOfTicTacToe = () => {
     return null;
 }
 
-// const winner = computed(() => winnerOfTicTacToe())
-// const newWinner = winner.value;
-// console.log(newWinner)
+
 
 
 
